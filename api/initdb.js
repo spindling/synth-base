4d982d55891e87b0508119ca882882caacdb70da
spindling <jeremy.sean009@gmail.com>
2026-03-28 13:59:35 -0400

    Created Synthesizers table with default entries

---
+++ api/initdb.js
@@ -0,0 +1,21 @@
+const sqlite3 = require("sqlite3").verbose();
+const sqlite = require("sqlite");
+
+async function startup(){
+
+    db = await sqlite.open({
+        filename: 'api.db',
+        driver: sqlite3.Database
+    });
+
+    await db.run("DROP TABLE IF EXISTS Synthesizers");
+    await db.run("CREATE TABLE Synthesizers (model TEXT, make TEXT, price NUMBER, keyboard INTEGER, type TEXT, voice TEXT)");
+    await db.run("INSERT INTO Synthesizers VALUES (?,?,?,?,?,?)", 
+        ["Moog", "MiniMoog", "7500", "1", "Analog", "Monophonic"]);
+    await db.run("INSERT INTO Synthesizers VALUES (?,?,?,?,?,?)", 
+        ["Yamaha", "DX7", "700", "1", "Digital", "Polyphonic"]);
+    await db.run("INSERT INTO Synthesizers VALUES (?,?,?,?,?,?)", 
+        ["ARP", "2600", "7500", "0", "Analog", "Monophonic"]);
+}
+
+startup();
