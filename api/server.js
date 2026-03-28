22275beb652a720631e3c3a1d66f9a65c600d539
spindling <jeremy.sean009@gmail.com>
2026-03-28 14:12:21 -0400

    Set up server.js with empty methods for api routes

---
+++ api/server.js
@@ -0,0 +1,48 @@
+const sqlite3 = require("sqlite3").verbose();
+const sqlite = require("sqlite");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+async function startup(){
+  db = await sqlite.open({
+    filename: 'api.db',
+    driver: sqlite3.Database
+});
+  
+  const server = app.listen(3000, function() {
+    console.log("RESTful API listening on port 3000");
+  });
+}
+
+app.get("/api", async function(req, res){
+    console.log("GET COLLECTION REQUEST RECEIVED");
+    const data = await db.all("SELECT rowid as id, * FROM Synthesizers");
+    res.json(data);
+});
+
+app.post("/api", async function(req,res){
+    console.log("POST TO COLLECTION REQUEST RECEIVED");
+    
+});
+
+app.delete("/api", async function(req, res){
+    console.log("DELETE COLLECTION REQUEST RECEIVED");
+
+});
+
+app.get("/api/:id", async function(req, res){
+    console.log("GET ITEM REQUEST RECEIVED");
+
+});
+
+app.put("/api/:id", async function(req, res){
+    console.log("PUT/MODIFY ITEM REQUEST RECEIVED")
+});
+
+app.delete("/api/:id", async function(req,res){
+    console.log("DELETE ITEM REQUEST RECEIVED");
+});
+
+//startup();
