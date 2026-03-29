102a9ce7881279130a700a592dcd3c50fa6b50c8
spindling <jeremy.sean009@gmail.com>
2026-03-29 17:03:49 -0400

    Move results to bottom of page

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -91,25 +91,7 @@ export default function RootLayout() {
 
     return (
       <View>
-        <View >
-          <Text style={styles.heading}>Results</Text>
-          <FlatList data={items} 
-                renderItem={({item}) => 
-                <View style={styles.listRow}>
-                
-                  <Text style={styles.listItem}>{item.make}</Text> 
-                  <Text style={styles.listItem}>{item.model} </Text>
-                  <Text style={styles.listItem}>{item.price} </Text> 
-                  <Text style={styles.listItem}>{item.keyboard} </Text> 
-                  <Text style={styles.listItem}>{item.type} </Text> 
-                  <Text style={styles.listItem}>{item.voice}</Text>
-                </View>}
-                keyExtractor={(item) => item.id }
-          />  
-           
-        </View>
-        
-        
+   
         <TouchableOpacity onPress={ retrieveAllItems }>
             <View style={styles.button}>
               <Text style={styles.buttonText}>Retrieve All Items
@@ -193,6 +175,24 @@ export default function RootLayout() {
                 </Text>
             </View>
         </TouchableOpacity>
+
+         <View >
+          <Text style={styles.heading}>Results</Text>
+          <FlatList data={items} 
+                renderItem={({item}) => 
+                <View style={styles.listRow}>
+                
+                  <Text style={styles.listItem}>{item.make}</Text> 
+                  <Text style={styles.listItem}>{item.model} </Text>
+                  <Text style={styles.listItem}>{item.price} </Text> 
+                  <Text style={styles.listItem}>{item.keyboard} </Text> 
+                  <Text style={styles.listItem}>{item.type} </Text> 
+                  <Text style={styles.listItem}>{item.voice}</Text>
+                </View>}
+                keyExtractor={(item) => item.id }
+          />  
+           
+        </View>
        
       </View>
     );
